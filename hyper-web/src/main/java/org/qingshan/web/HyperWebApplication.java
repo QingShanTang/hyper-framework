e3d2e7f737c0a29cb94f7c7e7d517f688db9b106
QingShanTang <1813764829@qq.com>
2022-03-18 10:56:06 +0800

    qingshan- hyper-web init

---
+++ hyper-web/src/main/java/org/qingshan/web/HyperWebApplication.java
@@ -0,0 +1,13 @@
+package org.qingshan.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperWebApplication.class, args);
+    }
+
+}
