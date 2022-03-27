cc00ffc8ce61b19604b2837610cc9c6494538898
QingShanTang <1813764829@qq.com>
2022-03-27 19:52:36 +0800

    qingshan- spring 自动注入工具

--- hyper-web/src/main/java/org/qingshan/web/HyperWebApplication.java
+++ hyper-web/src/main/java/org/qingshan/web/HyperWebApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"org.qingshan.dao"})
+@SpringBootApplication(scanBasePackages = {"org.qingshan"})
 @MapperScan(basePackages = {"org.qingshan.dao.mapper"})
 public class HyperWebApplication {
 
---
+++ hyper-web/src/main/java/org/qingshan/web/utils/SpringContextUtil.java
@@ -0,0 +1,28 @@
+package org.qingshan.web.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextUtil {
+
+    private static ApplicationContext applicationContext;
+
+    @Autowired
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextUtil.applicationContext = applicationContext;
+    }
+
+    /**
+     * 自动注入
+     *
+     * @param obj
+     */
+    public static <T> T autowireBean(T obj) {
+        AutowireCapableBeanFactory capableBeanFactory = applicationContext.getAutowireCapableBeanFactory();
+        capableBeanFactory.autowireBean(obj);
+        return obj;
+    }
+}
