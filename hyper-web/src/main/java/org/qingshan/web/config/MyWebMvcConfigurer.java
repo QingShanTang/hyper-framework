8ff1c8f6aa0d5c0cfec2a8d8f0d8a3de96c32876
QingShanTang <1813764829@qq.com>
2022-04-06 15:18:15 +0800

    qingshan- 国际化

--- hyper-web/src/main/java/org/qingshan/web/config/MyWebMvcConfigurer.java
+++ hyper-web/src/main/java/org/qingshan/web/config/MyWebMvcConfigurer.java
@@ -14,6 +14,7 @@ public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        //国际化拦截器
         registry.addInterceptor(localeChangeInterceptor)
                 .addPathPatterns("/api/**");
     }
