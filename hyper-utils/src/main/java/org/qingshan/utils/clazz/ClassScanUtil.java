0a41d45c159af254f409a3054f9c372f4af30400
QingShanTang <1813764829@qq.com>
2022-04-14 17:40:32 +0800

    qingshan- 类的扫描
    qingshan- 类的扫描
    
    qingshan- 类的扫描
    
    qingshan- 类的扫描
    
    qingshan- 类的扫描
    
    qingshan- 类的扫描

---
+++ hyper-utils/src/main/java/org/qingshan/utils/clazz/ClassScanUtil.java
@@ -0,0 +1,86 @@
+package org.qingshan.utils.clazz;
+
+import cn.hutool.core.util.ReUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@Slf4j
+public class ClassScanUtil {
+    public static List<Class> scan(
+            String className,
+            String[] excludeResources,
+            String... basePackages
+    ) {
+        List<Class> targetList = new ArrayList<>();
+
+        excludeResources = ArrayUtils.isEmpty(excludeResources) ? new String[]{} : Stream.of(excludeResources).filter(item -> {
+            return StringUtils.isNotBlank(item);
+        }).map(item -> {
+            return item.trim();
+        }).toArray(String[]::new);
+        basePackages = ArrayUtils.isEmpty(basePackages) ? new String[]{} : Stream.of(basePackages).filter(item -> {
+            return StringUtils.isNotBlank(item);
+        }).map(item -> {
+            return item.trim();
+        }).toArray(String[]::new);
+
+        if (StringUtils.isBlank(className) || ArrayUtils.isEmpty(basePackages)) {
+            return targetList;
+        }
+
+        for (String basePackage : basePackages
+        ) {
+            FilterBuilder filterBuilder = new FilterBuilder();
+            filterBuilder.includePackage(basePackage);
+            List<String> excludeResourcesRegexList = getExcludeResourcesRegexList(excludeResources);
+            if (CollectionUtils.isNotEmpty(excludeResourcesRegexList)) {
+                excludeResourcesRegexList.forEach(item -> {
+                    filterBuilder.exclude(item);
+                });
+            }
+
+            Reflections reflections = new Reflections(
+                    new ConfigurationBuilder()
+                            .addUrls(ClasspathHelper.forPackage(basePackage))
+                            .setScanners(new SubTypesScanner(false))
+                            .filterInputsBy(filterBuilder));
+
+            Set<Class<?>> scanedClass = reflections.getSubTypesOf(Object.class);
+            scanedClass.forEach(item -> {
+                if (ReUtil.isMatch(MessageFormat.format("^.+\\.[^.]*{0}[^.]*$", className), item.getTypeName())) {
+                    targetList.add(item);
+                }
+            });
+        }
+
+        return targetList;
+    }
+
+    private static List<String> getExcludeResourcesRegexList(String[] excludeResources) {
+        List<String> regexList = new ArrayList<>();
+        if (ArrayUtils.isNotEmpty(excludeResources)) {
+            Arrays.stream(excludeResources).forEach(item -> {
+                regexList.add(item.replace(".", "\\.") + ".*");
+            });
+        }
+        return regexList;
+    }
+
+    public static void main(String[] args) {
+        ClassScanUtil.scan("Class", null, "org.qingshan");
+    }
+}
