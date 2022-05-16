45e02dcc08707948f21ee032f2e11a464dde286b
QingShanTang <1813764829@qq.com>
2022-05-16 17:20:31 +0800

    qingshan-原生注解扫描

--- hyper-utils/src/main/java/org/qingshan/utils/annotationScan/NativeAnnotationScanUtil.java
+++ hyper-utils/src/main/java/org/qingshan/utils/annotationScan/NativeAnnotationScanUtil.java
@@ -1,7 +1,5 @@
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
@@ -16,10 +14,11 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 根据包名获取包的URL
+     *
      * @param pkgName com.demo.controller
      * @return
      */
-    public static String getPkgPath(String pkgName){
+    public static String getPkgPath(String pkgName) {
         String pkgDirName = pkgName.replace('.', File.separatorChar);
         URL url = Thread.currentThread().getContextClassLoader().getResource(pkgDirName);
 
@@ -28,17 +27,18 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 获取指定包下所有类对象的集合
-     * @param pkgName 包名(com.demo.controller)
-     * @param pkgPath 包路径(/Users/xxx/workspace/java/project/out/production/classes/com/demo/controller)
+     *
+     * @param pkgName   包名(com.demo.controller)
+     * @param pkgPath   包路径(/Users/xxx/workspace/java/project/out/production/classes/com/demo/controller)
      * @param recursive 是否递归遍历子目录
      * @return 类集合
      */
-    public static Set<Class<?>> scanClasses(String pkgName, String pkgPath, final boolean recursive){
+    public static Set<Class<?>> scanClasses(String pkgName, String pkgPath, final boolean recursive) {
         Set<Class<?>> classesSet = new HashSet<>();
 
         Collection<File> allClassFile = getAllClassFile(pkgPath, recursive);
 
-        for (File curFile : allClassFile){
+        for (File curFile : allClassFile) {
             try {
                 classesSet.add(getClassObj(curFile, pkgPath, pkgName));
             } catch (ClassNotFoundException e) {
@@ -51,24 +51,25 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 获取指定包下包含指定注解的所有类对象的集合
-     * @param pkgName 包名(com.demo.controller)
-     * @param pkgPath 包路径(/Users/xxx/workspace/java/project/out/production/classes/com/demo/controller)
-     * @param recursive 是否递归遍历子目录
+     *
+     * @param pkgName           包名(com.demo.controller)
+     * @param pkgPath           包路径(/Users/xxx/workspace/java/project/out/production/classes/com/demo/controller)
+     * @param recursive         是否递归遍历子目录
      * @param targetAnnotations 指定注解
      * @return 以注解和对应类集合构成的键值对
      */
     public static Map<Class<? extends Annotation>, Set<Class<?>>> scanClassesByAnnotations(
-            String pkgName, String pkgPath, final boolean recursive, List<Class<? extends Annotation>> targetAnnotations){
+            String pkgName, String pkgPath, final boolean recursive, List<Class<? extends Annotation>> targetAnnotations) {
         Map<Class<? extends Annotation>, Set<Class<?>>> resultMap = new HashMap<>(16);
 
         Collection<File> allClassFile = getAllClassFile(pkgPath, recursive);
 
-        for (File curFile : allClassFile){
+        for (File curFile : allClassFile) {
             try {
                 Class<?> curClass = getClassObj(curFile, pkgPath, pkgName);
-                for (Class<? extends Annotation> annotation : targetAnnotations){
-                    if (curClass.isAnnotationPresent(annotation)){
-                        if (!resultMap.containsKey(annotation)){
+                for (Class<? extends Annotation> annotation : targetAnnotations) {
+                    if (curClass.isAnnotationPresent(annotation)) {
+                        if (!resultMap.containsKey(annotation)) {
                             resultMap.put(annotation, new HashSet<Class<?>>());
                         }
                         resultMap.get(annotation).add(curClass);
@@ -84,13 +85,14 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 加载类
+     *
      * @param file
      * @param pkgPath
      * @param pkgName
      * @return
      * @throws ClassNotFoundException
      */
-    private static Class<?> getClassObj(File file, String pkgPath, String pkgName) throws ClassNotFoundException{
+    private static Class<?> getClassObj(File file, String pkgPath, String pkgName) throws ClassNotFoundException {
         // 考虑class文件在子目录中的情况
         String absPath = file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - EXT.length() - 1);
         String className = absPath.substring(pkgPath.length()).replace(File.separatorChar, '.');
@@ -101,14 +103,15 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 遍历指定目录下所有扩展名为class的文件
-     * @param pkgPath 包目录
+     *
+     * @param pkgPath   包目录
      * @param recursive 是否递归遍历子目录
      * @return
      */
-    private static Collection<File> getAllClassFile(String pkgPath, boolean recursive){
+    private static Collection<File> getAllClassFile(String pkgPath, boolean recursive) {
         File fPkgDir = new File(pkgPath);
 
-        if (!(fPkgDir.exists() && fPkgDir.isDirectory())){
+        if (!(fPkgDir.exists() && fPkgDir.isDirectory())) {
             log.error("the directory to package is empty: {}", pkgPath);
 
             return null;
@@ -119,21 +122,22 @@ public class NativeAnnotationScanUtil {
 
     /**
      * 查找指定注解的Method
-     * @param classes 查找范围
+     *
+     * @param classes           查找范围
      * @param targetAnnotations 指定的注解
      * @return 以注解和对应Method类集合构成的键值对
      */
     public static Map<Class<? extends Annotation>, Set<Method>> scanMethodsByAnnotations(Set<Class<?>> classes,
-                                                                                         List<Class<? extends Annotation>> targetAnnotations){
+                                                                                         List<Class<? extends Annotation>> targetAnnotations) {
         Map<Class<? extends Annotation>, Set<Method>> resultMap = new HashMap<>(16);
 
-        for (Class<?> cls : classes){
+        for (Class<?> cls : classes) {
             Method[] methods = cls.getMethods();
 
-            for (Class<? extends Annotation> annotation : targetAnnotations){
-                for (Method method : methods){
-                    if (method.isAnnotationPresent(annotation)){
-                        if (!resultMap.containsKey(annotation)){
+            for (Class<? extends Annotation> annotation : targetAnnotations) {
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(annotation)) {
+                        if (!resultMap.containsKey(annotation)) {
                             resultMap.put(annotation, new HashSet<Method>());
                         }
                         resultMap.get(annotation).add(method);
