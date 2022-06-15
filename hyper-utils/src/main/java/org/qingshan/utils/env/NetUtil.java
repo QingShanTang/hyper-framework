b4172b5867b0faba7ff753e9037bd8f4d6700968
QingShanTang <1813764829@qq.com>
2022-06-15 11:25:36 +0800

    qingshan-mac地址获取

--- hyper-utils/src/main/java/org/qingshan/utils/env/NetUtil.java
+++ hyper-utils/src/main/java/org/qingshan/utils/env/NetUtil.java
@@ -1,5 +1,7 @@
 package org.qingshan.utils.env;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
@@ -11,7 +13,19 @@ import java.util.stream.Collectors;
 /**
  * 网络工具
  */
+@Slf4j
 public class NetUtil {
+
+    public static List<String> macs;
+
+    static {
+        try {
+            macs = getMacList();
+        } catch (Exception e) {
+            log.error("获取mac地址失败,errorMsg->", e);
+        }
+    }
+
     /**
      * 获取mac地址
      * 一台机器可能有多个网卡
@@ -19,9 +33,9 @@ public class NetUtil {
      * @return
      * @throws Exception
      */
-    public static List<String> getMacList() throws Exception {
+    private static List<String> getMacList() throws Exception {
+        log.info("获取mac地址。。。");
         Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
-        StringBuilder sb = new StringBuilder();
         ArrayList<String> tmpMacList = new ArrayList<>();
         while (en.hasMoreElements()) {
             NetworkInterface iface = en.nextElement();
@@ -36,7 +50,7 @@ public class NetUtil {
                 if (mac == null) {
                     continue;
                 }
-                sb.delete(0, sb.length());
+                StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < mac.length; i++) {
                     sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
                 }
@@ -48,14 +62,8 @@ public class NetUtil {
         }
         /***去重，别忘了同一个网卡的ipv4,ipv6得到的mac都是一样的，肯定有重复，下面这段代码是。。流式处理***/
         List<String> unique = tmpMacList.stream().distinct().collect(Collectors.toList());
+        log.info("mac:{}", unique);
         return unique;
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("进行 multi net address 测试===》");
-        List<String> macs = getMacList();
-        System.out.println("本机的mac网卡的地址有：" + macs);
-
     }
 }
 
---
+++ hyper-utils/src/test/java/org/qingshan/utils/env/TestNetUtil.java
@@ -0,0 +1,12 @@
+package org.qingshan.utils.env;
+
+import org.junit.Test;
+
+public class TestNetUtil {
+
+    @Test
+    public void testGetMacs() {
+        System.out.println(NetUtil.macs);
+        System.out.println(NetUtil.macs);
+    }
+}
