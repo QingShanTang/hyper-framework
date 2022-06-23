20b865d96217df79a667a0e821235d6abbd8d82f
QingShanTang <1813764829@qq.com>
2022-06-23 11:11:04 +0800

    qingshan-

--- hyper-web/src/main/java/org/qingshan/web/utils/job/anno/JobExecutor.java
+++ hyper-web/src/main/java/org/qingshan/web/utils/job/anno/JobExecutor.java
@@ -6,6 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 针对注解切面
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JobExecutor {
